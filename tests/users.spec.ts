f998ccf9bdae0910262bca40ff2cc6147fe9d7c2
Kateryna Yeromenko <kateryna.yeromenko.qa@gmail.com>
2026-08-26 12:44:21 +0200

    test: add existing user retrieval API test

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -28,3 +28,33 @@ test('API-USERS-001 - should retrieve users collection', async ({ request }) => 
   expect(headers).toHaveProperty('x-pagination-total');
   expect(headers).toHaveProperty('x-pagination-pages');
 });
+
+test('API-USERS-002 - should retrieve existing user by ID', async ({ request }) => {
+  const usersResponse = await request.get('users');
+
+  expect(usersResponse.status()).toBe(200);
+
+  const users = await usersResponse.json();
+
+  expect(Array.isArray(users)).toBe(true);
+  expect(users.length).toBeGreaterThan(0);
+
+  const existingUserId = users[0].id;
+
+  const response = await request.get(`users/${existingUserId}`);
+
+  expect(response.status()).toBe(200);
+
+  const contentType = response.headers()['content-type'];
+  expect(contentType).toContain('application/json');
+
+  const user = await response.json();
+
+  expect(user).toHaveProperty('id');
+  expect(user).toHaveProperty('name');
+  expect(user).toHaveProperty('email');
+  expect(user).toHaveProperty('gender');
+  expect(user).toHaveProperty('status');
+
+  expect(user.id).toBe(existingUserId);
+});
